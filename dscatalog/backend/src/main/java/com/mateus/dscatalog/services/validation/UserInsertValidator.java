236840c920f2600e62d2c1a046785d657f7a86b9
Mateus Vinícius dos Passos <mviniciusdospassos@gmail.com>
2021-07-17 15:44:08 -0300

    (#09) Constraint Validator

---
+++ dscatalog/backend/src/main/java/com/mateus/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,42 @@
+package com.mateus.dscatalog.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.mateus.dscatalog.domain.User;
+import com.mateus.dscatalog.dtos.UserInsertDTO;
+import com.mateus.dscatalog.repositories.UserRepository;
+import com.mateus.dscatalog.resources.exceptions.FieldMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid ann) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO dto, ConstraintValidatorContext context) {
+
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = userRepository.findByEmail(dto.getEmail());
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email already in use"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
